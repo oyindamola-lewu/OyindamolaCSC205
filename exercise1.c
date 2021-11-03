f203190afb551a0d1d7b3fd5576fa888e53fbbb6
Oyindamola Lewu <oyindamola.lewu@pau.edu.ng>
2021-11-03 15:11:46 +0100

    initial commit

---
+++ exercise1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("C Programming is a powerful general-purpose language.\n");
+	printf("It is an excellent language to learn how to program.\n");
+	return 0;
+}
